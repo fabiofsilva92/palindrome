70b864090714e366b9944aa0c456e78a121640a7
fabiofsilva92 <fabiofsilva92@gmail.com>
2020-11-12 15:45:19 -0300

    Primeiro Up - Palindromo

---
+++ src/palindromo.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JOptionPane;
+import javax.swing.*; 
+
+
+public class palindromo {
+    
+    public static void main (String args[])
+    {
+        
+        String palidromo = JOptionPane.showInputDialog("Digite a palavra para verificação se é palíndromo: ");
+        
+        char [] invertido = palindromo.toCharArray();
+        
+        
+    }
+    
+}
